3785e38ccb3f0e8f907c72e5e9d209550d93428a
hyun <hgh6128@gmail.com>
2023-03-16 18:08:40 +0900

    상세 정보 입력 페이지 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Nav from 'components/Nav';
 import Detail from 'pages/details/Detail';
-
 import Weight from 'pages/guide/Weight';
 import Game from 'pages/game/Game';
 import Login from 'pages/login/Login';
@@ -10,7 +9,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Wrapper } from 'styles/LayoutCss';
 import './App.css';
 import statusBar from 'assets/statusBar.png';
-
+import DetailInfo from 'pages/login/DetailInfo';
 function App() {
   return (
     <BrowserRouter>
@@ -19,10 +18,11 @@ function App() {
         <Routes>
           <Route path='/game' element={<Game />} />
           <Route path='/detail' element={<Detail />} />
+          <Route path='/detailinfo' element={<DetailInfo />} />
           <Route path='/userinfo' element={<UserInfo />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
-             <Route path='/detail' element={<Detail />} />
+          <Route path='/detail' element={<Detail />} />
           <Route path='/Weight' element={<Weight />} />
         </Routes>
         <Nav />
@@ -30,5 +30,4 @@ function App() {
     </BrowserRouter>
   );
 }
-
 export default App;
